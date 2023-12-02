e380e85615a1b6b3a6af6b1412b8b3c56901f772
sileanth <lukasz.magnuszewski@gmail.com>
2023-12-02 10:25:59 +0100

    migration to astro

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+  inputs = {
+	nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  };
+
+ 
+  outputs = {self, nixpkgs, ... }: 
+  let 
+    pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true;};
+  in {
+    	devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+      ] ;
+
+    };
+
+
+    };
+
+
+}
